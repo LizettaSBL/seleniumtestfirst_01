9d6ad23a3ba8700eb8ff487a34f6a57fafd422c3
LizettaSBL <lizettasbl@gmail.com>
2021-09-12 14:56:43 +0300

    Admin Login Test. Task 3

---
+++ src/test/java/ru/org/autotest/LoginAdminPanel.java
@@ -0,0 +1,48 @@
+package ru.org.autotest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginAdminPanel {
+
+    private static WebDriver driver;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
+        driver = new ChromeDriver();
+        System.out.println("Test is started");
+
+    }
+
+    @Test
+    public void setup() {
+        driver.get("http://localhost/litecart/admin/login.php");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        String actualUrl = "http://localhost/litecart/admin/";
+        String expectedUrl = driver.getCurrentUrl();
+        if (actualUrl.equalsIgnoreCase(expectedUrl)) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+    }
+
+
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+
+
+}
