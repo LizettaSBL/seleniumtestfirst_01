117ffe20296b0cd449567091e62344b2c772406c
LizettaSBL <lizettasbl@gmail.com>
2021-09-28 01:57:43 +0300

    Task 9. 1 a+b

--- src/test/java/ru/org/autotest/SortingCountries.java
+++ src/test/java/ru/org/autotest/SortingCountries.java
@@ -35,27 +35,51 @@ public class SortingCountries {
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
 
-
+        //1-a
         List<WebElement> listCountries = driver.findElements(By.cssSelector("tr.row td a:not([title=Edit])"));
         List<String> Countries = new ArrayList<>();
         List<String> textContents = new ArrayList<>();
 
-        for ( var listCountry:listCountries) {
+        for (var listCountry : listCountries) {
             var textContent = listCountry.getAttribute("textContent");
             Countries.add(textContent);
             textContents.add(textContent);
         }
         Collections.sort(Countries);
-        Countries.equals(textContents);
 
+        if (Countries.equals(textContents)==true) {
+            System.out.println("sorting of countries is correct");
+        }
+
+
+        //1-b
+        List<WebElement> countryRow = driver.findElements(By.cssSelector("tr.row"));
+        for (int i = 2; i <= countryRow.size(); i++) {
+            if (!driver.findElement(By.cssSelector("tr:nth-child("+i+") > td:nth-child(6)")).getText().equals("0")) {
+
+                driver.findElement(By.cssSelector(" tr:nth-child("+i+") > td:nth-child(5)> a ")).click();
+
+                List<WebElement> listZones = driver.findElements(By.cssSelector("td:nth-child(3)>input[name^='zones']"));
+                List<String> namesZones = new ArrayList<>();
+                List<String> textZones = new ArrayList<>();
+                for (var listZona : listZones) {
+                    var textZona = listZona.getAttribute("value");
+                    namesZones.add(textZona);
+                    textZones.add(textZona);
+                }
+                Collections.sort(namesZones);
+                if (namesZones.equals(textZones)==true){
+                    System.out.println("sorting is correct");
+                }
+                driver.get("http://localhost/litecart/admin/?app=countries&doc=countries");
+            }
+        }
     }
 
-
-
-    @After
-    public void stop() {
-        System.out.println("Test is ended");
-        driver.quit();
-        driver = null;
-}
-}
+        @After
+        public void stop () {
+            System.out.println("Test is ended");
+            driver.quit();
+            driver = null;
+        }
+    }
