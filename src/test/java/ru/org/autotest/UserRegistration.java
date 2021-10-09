c54940c25580c1f977241c815fd941e78498b521
LizettaSBL <lizettasbl@gmail.com>
2021-10-09 19:51:49 +0300

    Task11

---
+++ src/test/java/ru/org/autotest/UserRegistration.java
@@ -0,0 +1,102 @@
+package ru.org.autotest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+/*      1) регистрация новой учётной записи с уникальным адресом
+        2) выход(logout)
+        3) повторный вход в только что созданную учётную запись,и ещё раз выход.
+        4) Страна - United States,штат произвольный.Формат индекса-пять цифр.*/
+
+public class UserRegistration {
+    private static WebDriver driver;
+
+    private static final String firstName = "Elizaveta";
+    private static final String lastName = "Sobol";
+    public static final String address = "Hauptstrasse 1,1";
+    public static final String postcode = "12345";
+    public static final String city = "Chicago";
+    public static final String country = "United States";
+    public static final String phone = "8002633394";
+    public static final String password = "password";
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println("Test is started");
+
+    }
+
+    @Test
+    public void Task11Test() throws InterruptedException {
+        driver.get("http://localhost/litecart/en/");
+        driver.findElement(By.cssSelector("#box-account-login tr:nth-child(5) a")).click();
+        WebElement createAccount = driver.findElement(By.cssSelector("#create-account"));
+//Entering a value captcha
+        String captchaVal = JOptionPane.showInputDialog("Please enter the captcha value:");
+        driver.findElement(By.xpath("//*[@id=\"create-account\"]/div//input[2]")).sendKeys(captchaVal);
+//Entering values
+        setField(createAccount, "firstname", firstName);
+        setField(createAccount, "lastname", lastName);
+        setField(createAccount, "address1", address);
+        setField(createAccount, "postcode", postcode);
+        setField(createAccount, "city", city);
+//Country entry
+        WebElement selectCountry = createAccount.findElement(By.tagName("select"));
+        Select select = new Select(selectCountry);
+        select.selectByVisibleText(country);
+//generating mail
+        ((JavascriptExecutor) driver).executeScript("window.open()");
+        ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+        driver.get("https://yopmail.com/ru/email-generator");
+        String email = driver.findElement(By.xpath("//*[@id=\"egen\"]")).getText();
+        driver.switchTo().window(tabs.get(0)); // switch back to main screen
+//Entering values
+        setField(createAccount, "email", email);
+        setField(createAccount, "phone", phone);
+        setField(createAccount, "password", password);
+        setField(createAccount, "confirmed_password", password);
+//Create an account
+        createAccount.findElement(By.cssSelector("button[name=create_account]")).click();
+//logout
+        logout();
+
+//account login
+        WebElement login = driver.findElement(By.cssSelector("#box-account-login"));
+        setField(login, "email", email);
+        setField(login, "password", password);
+        login.findElement(By.cssSelector("button[name=login]")).click();
+//logout
+        logout();
+    }
+
+
+    private void logout() {
+        driver.findElement(By.cssSelector("#box-account li:nth-child(4) a")).click();
+    }
+
+    private void setField(WebElement createAccount, String field, String data) {
+        createAccount.findElement(By.cssSelector("input[name=" + field + "]")).sendKeys(data);
+    }
+
+
+    @After
+    public void stop() {
+        System.out.println("Test is ended");
+        driver.quit();
+        driver = null;
+    }
+
+
+}
